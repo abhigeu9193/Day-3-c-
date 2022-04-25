f3f18b9925692ae7dc10ff08aae4c5b023b1c4b2
Abhishek <abhigeu9193@gmail.com>
2022-04-25 10:51:09 +0530

    Initial Commit

---
+++ program1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class program1
+{
+
+    static void Main(string[] args)
+    {
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is persent");
+        }
+        else
+        {
+            Console.WriteLine("Employee is not persent");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
