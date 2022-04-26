ced607f4d2fcbb8571c4187b2f63965a9101d67b
Abhishek <abhigeu9193@gmail.com>
2022-04-26 23:40:39 +0530

    [geometry] problem2

---
+++ geometry2/geometry2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+class geometry2
+{
+    private static int axisCheck;
+
+    static void Main(string[] args)
+    {
+        int x = 0, y = 0;
+
+
+        Random random = new Random();
+        int xaxisCheck = random.Next(0, 2);
+        int yaxisCheck = random.Next(0, 2);
+        Console.WriteLine(random);
+        if (xaxisCheck == yaxisCheck)
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+
+
+
+    }
+}
