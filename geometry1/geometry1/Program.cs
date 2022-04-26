24543a17d2a06c7aca64b67a6976967ba6e92c36
Abhishek <abhigeu9193@gmail.com>
2022-04-26 23:34:56 +0530

    [geometry]problem1

---
+++ geometry1/geometry1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+//using System.Math;
+class geometry1
+{
+    private static int axisCheck;
+
+    static void Main(string[] args)
+    {
+        double x1=1,x2=2,y1=1,y2=2;
+        double length;
+        length = Math.Sqrt(x2 - x1)+ Math.Pow(2,4) + Math.Sqrt(y2 - y1)+ Math.Pow(2,4);
+            
+        Console.WriteLine(length);
+        
+       
+
+        
+
+    }
+
+}
